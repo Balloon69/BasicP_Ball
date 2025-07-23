577b525021a2e95fde53ee369d025d6660100756
Balloon <ball.03062550@gmail.com>
2025-07-23 09:35:44 +0700

    Nested if

--- BasicP_class_2_worhshop.py
+++ BasicP_class_2_worhshop.py
@@ -1,14 +1,27 @@
-dis = int(input("ระยะทาง :"))
-if dis >= 500:
-    print("ราคา : 45 บาท")
-elif dis >= 301 and dis <= 500:
-    print("ราคา : 35 บาท")
-elif dis>= 101 and dis<=300:
-    print("ราคา : 25 บาท")
-elif dis>= 51 and dis<=100:
-    print("ราคา : 15 บาท")
-elif dis>=5 and dis<=50:
-    print("ราคา : 10 บาท")
-else:
-    print("ส่งฟรี")
+# dis = int(input("ระยะทาง :"))
+# if dis >= 500:
+#     print("ราคา : 45 บาท")
+# elif dis >= 301 and dis <= 500:
+#     print("ราคา : 35 บาท")
+# elif dis>= 101 and dis<=300:
+#     print("ราคา : 25 บาท")
+# elif dis>= 51 and dis<=100:
+#     print("ราคา : 15 บาท")
+# elif dis>=5 and dis<=50:
+#     print("ราคา : 10 บาท")
+# else:
+#     print("ส่งฟรี")
     
+username = input("ชื่อผู้ใช้ :")
+password = input("รหัสผ่าน :")
+if username == "Ball" and password == "1234":
+    print("เข้าสู่ระบบสำเร็จ")
+    ser = input("ป้อนหมายเลขบริการ :")
+    if ser == "1":
+        print("ถอนเงิน")
+    elif ser == "2":
+        print("ฝากเงิน")
+    elif ser == "3":
+        print("หมายเลขไม่ถูกต้อง")
+else:
+    print("เข้าสู่ระบบไม่สำเร็จ")
