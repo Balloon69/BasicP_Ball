a04ef72e75325730405f7b436371269c54dfb21f
Balloon <ball.03062550@gmail.com>
2025-07-25 11:58:29 +0700

    ร้านพี่ปลิ้ม

--- BasicP_class4_workshop.py
+++ BasicP_class4_workshop.py
@@ -1,20 +1,32 @@
 # ฟังก์ชันแสดงรายชื่อหนังทั้งหมดในระบบ
+movies = [
+    {'movie_name': 'Avengers Endgame', 'ticket_price': 300, 'genre': 'Action', 'age_restriction': '13'},
+    {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13'},
+    {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18'},
+    {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13'},
+    {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'G'}
+]
 def show_movies(movie_list):
     # TODO: วนลูปแสดงชื่อหนังพร้อมราคาตั๋ว
     for i in  movie_list:
-        return(movie_list(['movie_name','ticket_price']))
-print(show_movies)
+        print(i['movie_name'],i['ticket_price'])
+    
+print(show_movies(movies))
+
 # # ฟังก์ชันตรวจสอบอายุตามข้อจำกัดของหนัง
 def check_age(user_age, age_restriction):
     # TODO: ถ้า age_restriction เป็น 'G' ให้ผ่านเลย
     # ถ้าไม่ใช่ ให้ดึงเลขอายุขั้นต่ำมาเปรียบเทียบกับ user_age
-    if age_restriction == 'G':
-        print("pass")
-    else:
-        if user_age >= age_restriction:
-            print("ดูได้")
+    for age in (user_age):
+        user_age = input("Enter age :")
+        if age_restriction == 'G':
+            print("pass")
+        elif user_age >= age_restriction:
+                print("ดูได้")
         else:
             print("ดูไม่ได้")
+
+print(check_age)
 
 # # ฟังก์ชันคำนวณราคาตั๋วโดยขึ้นกับประเภทหนัง
 def calculate_price(base_price, genre):
@@ -26,7 +38,7 @@ def calculate_price(base_price, genre):
         return base_price
 
 # # ฟังก์ชันสำหรับการซื้อบัตรชมหนัง
-def buy_ticket(movie_list):
+# def buy_ticket(movie_list):
 #     # TODO: 
 #     # 1. เรียก show_movies เพื่อแสดงรายชื่อหนัง
 #     # 2. รับค่าตัวเลือกหนังจากผู้ใช้ (1-5)
@@ -37,15 +49,9 @@ def buy_ticket(movie_list):
 #     # 6. คำนวณราคาตั๋วโดยใช้ calculate_price
 #     # 7. แสดงผลการซื้อบัตร พร้อมชื่อหนัง, เสียงที่เลือก, ราคาตั๋ว
 
-def main():
-#     # TODO: สร้างรายการหนังเป็น list ของ dict โดยเก็บข้อมูล movie_name, ticket_price, genre, age_restriction
-    movies = [
-        {'movie_name': 'Avengers Endgame', 'ticket_price': 300, 'genre': 'Action', 'age_restriction': '13'},
-        {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13'},
-        {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18'},
-        {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13'},
-        {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'G'}
-    ]
+# def main():
+    # TODO: สร้างรายการหนังเป็น list ของ dict โดยเก็บข้อมูล movie_name, ticket_price, genre, age_restriction
+
 
 #     # TODO: แสดงเมนูให้ผู้ใช้เลือก
 #     # 1. แสดงหนังทั้งหมด
--- BasicP_class_4.py
+++ BasicP_class_4.py
@@ -60,10 +60,92 @@
 #     print("not pass")
 
 
-students = [{"name": "Thanasorn Dusadeeroj", "age": 19, "ID": 67130500014},
-            {"name": "satit", "age": 15, "ID": 6713050047}]
-for student in students:
-    if student["age"] >= 18:
-        print("เกิน")
+# students = [{"name": "Thanasorn Dusadeeroj", "age": 19, "ID": 67130500014},
+#             {"name": "satit", "age": 15, "ID": 6713050047}]
+# for student in students:
+#     if student["age"] >= 18:
+#         print("เกิน")
+#     else:
+#         print("ไม่เกิน")
+
+
+
+
+food = [
+    {"number": "1", "name_food": "กะเพราหมูกรอบ", "meat": "หมุกรอบ", "price": 600},
+    {"number": "2", "name_food": "กะเพราหมู", "meat": "หมู", "price": 550},
+    {"number": "3", "name_food": "กะเพราไก่", "meat": "ไก่", "price": 500},
+    {"number": "4", "name_food": "กะเพราเนื้อ", "meat": "เนื้อ", "price": 900},
+    {"number": "5", "name_food": "กะเพราทะเล", "meat": "กุ้ง+หมึก", "price": 800}
+]
+
+# แสดงเมนูอาหารภายในร้าน
+def show_manu(m):
+    for i in m:
+        print(i["number"], "ชื่ออาหาร", i['name_food'], "ราคา", i['price'])
+
+print(show_manu(food))
+
+print("---------------------------------------------------------------------")
+
+# รับคำสั่งอาหาร
+manu_food = input("สั่งอาหาร :")
+if manu_food == "1":
+    egg = input("เพิ่มไข่ดาวไหม(1.เพิ่ม/2.ไม่เพิ่ม) :")
+    if egg == "1":
+        p = 600 + 150
     else:
-        print("ไม่เกิน")
+        p = 600
+elif manu_food == "2":
+    egg = input("เพิ่มไข่ดาวไหม(1.เพิ่ม/2.ไม่เพิ่ม) :")
+    if egg == "1":
+        p = 550 + 150
+    else:
+        p = 550
+elif manu_food == "3":
+    egg = input("เพิ่มไข่ดาวไหม(1.เพิ่ม/2.ไม่เพิ่ม) :")
+    if egg == "1":
+        p = 500 + 150
+    else:
+        p = 500
+elif manu_food == "4":
+    egg = input("เพิ่มไข่ดาวไหม(1.เพิ่ม/2.ไม่เพิ่ม) :")
+    if egg == "1":
+        p = 900 + 150
+    else:
+        p = 900
+elif manu_food == "5":
+    egg = input("เพิ่มไข่ดาวไหม(1.เพิ่ม/2.ไม่เพิ่ม) :")
+    if egg == "1":
+        p = 800 + 150
+    else:
+        p = 800
+else:
+    print("ไม่มีเมนูนี้")
+    exit()
+
+print("ราคา:", p, "บาท")
+
+print("---------------------------------------------------------------------")
+
+# รับวิธีกิน
+take = input("1กินที่ร้าน, 2กลับบ้าน: ")
+if take == "1":
+    print("กินร้าน")
+else:
+    print("กลับบ้าน")
+
+print("---------------------------------------------------------------------")
+
+# วิธีจ่ายเงิน
+pay = input("1.สแกน หรือ 2.จ่ายเงินสด: ")
+if pay == "2":
+    money = int(input("เงิน : "))
+    if money >= p:
+        cal = money - p
+        print("เงินทอน:", cal, "บาท")
+    else:
+        print("ยอดเงินไม่พอ ขาดอีก", p - money, "บาท")
+else:
+    print("กรุณาสแกนจ่ายเงินจำนวน", p, "บาท ขอบคุณค่ะ")
+
